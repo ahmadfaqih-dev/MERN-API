e3deaacd9ecfc6f994880f44292a460a457d128a
ahmadfaqih-dev <faqih.elekyoben@gmail.com>
2020-12-14 18:02:29 +0700

    Memahamai dan membuat Router Request Response

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 
-const app =express();
+const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('hello server..');
-    console.log('Hello kawan');
+router.use('/products', (req, res, next) => {
+    console.log('url: ', req.originalUrl);
+    console.log('method: ', req.method);
+    res.json({name: "ahmad faqih", email: "ahamadfaqih@gmail.com"});
+    next();
 })
 
+router.use('/price', (req, res, next) => {
+    res.json({price: 3000000});
+    next();
+})
+
+router.get('/customers', (req, res, next) => {
+    res.json({title: "Customers"});
+    next();
+})
+
+app.use('/', router);
+
+// GET '/user' ==> [{name: faqih}]
+ 
 app.listen(4000);
